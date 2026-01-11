dc494e5aabaaa3012f36ab00c4dcce65b14bf26c
IamRitwik <iamritwik01@gmail.com>
2026-01-11 22:47:00 +0530

    added comments

--- 5.Sliding-Window/1.2.Dynamic-Window/longest_sub-string_without_repeating_chars.py
+++ 5.Sliding-Window/1.2.Dynamic-Window/longest_sub-string_without_repeating_chars.py
@@ -6,11 +6,14 @@ def longest_substring_with_unique_chars(s: str) -> int:
    hash_set = set()
    left = right = 0
    while right < len(s):
+        # duplicate found
         while s[right] in hash_set:
             hash_set.remove(s[left])
+            # shrink window
             left += 1
         max_len = max(max_len, right - left + 1)
         hash_set.add(s[right])
+        # expand window
         right += 1
 
    return max_len 
