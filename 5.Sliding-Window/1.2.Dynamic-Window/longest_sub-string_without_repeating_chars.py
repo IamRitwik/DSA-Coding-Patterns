0b37626a9c08874f100e82479119609658e6366e
IamRitwik <iamritwik01@gmail.com>
2026-01-11 22:43:21 +0530

    added longest sub string without repeating characters

---
+++ 5.Sliding-Window/1.2.Dynamic-Window/longest_sub-string_without_repeating_chars.py
@@ -0,0 +1,22 @@
+# Time - O(N)
+# Space - O(M)
+def longest_substring_with_unique_chars(s: str) -> int:
+   # Write your code here
+   max_len = 0
+   hash_set = set()
+   left = right = 0
+   while right < len(s):
+        while s[right] in hash_set:
+            hash_set.remove(s[left])
+            left += 1
+        max_len = max(max_len, right - left + 1)
+        hash_set.add(s[right])
+        right += 1
+
+   return max_len 
+
+
+
+if __name__ == "__main__":
+   s = "abcba"
+   print(longest_substring_with_unique_chars(s))
