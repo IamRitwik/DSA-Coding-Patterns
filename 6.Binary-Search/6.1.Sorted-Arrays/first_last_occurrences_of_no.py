7ad58c9b3cb38e2e262ac58c33e47bcb5abb59ea
IamRitwik <iamritwik01@gmail.com>
2026-01-15 22:09:34 +0530

    day 14 streak: binary search lower/upper boundary

---
+++ 6.Binary-Search/6.1.Sorted-Arrays/first_last_occurrences_of_no.py
@@ -0,0 +1,50 @@
+from typing import List
+
+def first_and_last_occurrences_of_a_number(nums: List[int], target: int) -> List[int]:
+    # Write your code here
+    if not nums:
+        return [-1,-1]
+    
+    first_occur = lower_bound(nums, target)
+
+    if first_occur >= len(nums) or nums[first_occur] != target:
+        return [-1,-1]
+    
+    last_occur = upper_bound(nums, target) - 1
+
+    return [first_occur, last_occur]
+
+    
+def lower_bound(nums: List[int], target: int):
+    left, right = 0 , len(nums)
+
+    while left < right:
+        mid = (left + right) // 2  # Calculate mid INSIDE the loop
+        if nums[mid] >= target:
+            right = mid
+        else:
+            left = mid + 1
+    return left
+
+def upper_bound(nums: List[int], target: int):
+    left, right = 0, len(nums)
+
+    while left < right:
+        mid = (left + right) // 2  # Calculate mid INSIDE the loop
+        if nums[mid] > target:
+            right = mid
+        else:
+            left = mid + 1
+    return left
+        
+
+if __name__ == "__main__":
+    nums = [1, 2, 3, 4, 4, 4, 5, 6, 7, 8, 9, 10, 11]
+    target = 4
+
+    print(first_and_last_occurrences_of_a_number(nums, target))
+
+    nums = [1, 2, 4, 7, 9]
+    target = 4
+
+    print(first_and_last_occurrences_of_a_number(nums, target))
