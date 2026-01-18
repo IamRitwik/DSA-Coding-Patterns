331e17c9b8393dd640c327bae09c7f3da36513a0
IamRitwik <iamritwik01@gmail.com>
2026-01-18 19:43:52 +0530

    day 17 streak: binary search on rotated sorted array

---
+++ 6.Binary-Search/6.3.Partially-Sorted-Array/search_in_rotated_sorted_array.py
@@ -0,0 +1,35 @@
+from typing import List
+
+def find_the_target_in_a_rotated_sorted_array(nums: List[int], target: int) -> int:
+    # Write your code here
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+
+        # check if left sub array is sorted
+        # and target is in the rane
+        elif nums[left] <= nums[mid]:
+
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        else:    
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+    return - 1            
+                 
+
+
+if __name__ == "__main__":
+    nums = [8, 9, 1, 2, 3, 4, 5, 6, 7]
+    target = 1
+
+    print(find_the_target_in_a_rotated_sorted_array(nums, target))
