62d6030b30bd5d1d711dfd9ec294ed679fc9e4a3
IamRitwik <iamritwik01@gmail.com>
2026-01-31 15:03:56 +0530

    added iterative solution

--- 11.Trees/DFS/1.invert_binary_tree.py
+++ 11.Trees/DFS/1.invert_binary_tree.py
@@ -17,3 +17,22 @@ def invert_binary_tree(root: TreeNode) -> TreeNode:
     invert_binary_tree(root.right)
 
     return root
+
+
+def invertTree(root: TreeNode) -> TreeNode:
+    if not root:
+        return None
+    
+    stack = [root]
+
+    while stack:
+        node = stack.pop()
+
+        node.left, node.right = node.right, node.left
+
+        if node.left:
+            stack.append(node.left)
+        if node.right:
+            stack.append(node.right)
+
+    return root
